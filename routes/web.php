0e69b3936a45fd4cfd6ad523f19c9fb93b65208c
Your Name <you@example.com>
2023-11-23 14:55:14 +0700

    Pertemuan tgl 23 Nop 2023

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,10 @@ Route::get('week1',function (){
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
 
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
